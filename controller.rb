911d9b7b24ab5f83c0abdc99a002677137c95cf8
lawrie-sm <alertrelic@gmail.com>
2019-07-18 11:43:09 +0100

    Added form to create students

--- controller.rb
+++ controller.rb
@@ -3,7 +3,11 @@ require 'sinatra/reloader'
 require_relative './models/student'
 also_reload './models/*'
 
-get '/' do
+get '/students' do
   @students = Student.all
   erb(:students)
+end
+
+get '/students/new' do
+  erb(:new_student)
 end
