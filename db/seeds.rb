6003e9a7a203002899b58be5d96c47fe23b866d0
lawrie-sm <alertrelic@gmail.com>
2019-07-18 12:02:53 +0100

    Added houses to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,27 @@
 require_relative '../models/student'
+require_relative '../models/house'
+
+House.delete_all
+gryffindor = House.new(
+  'name' => 'Gryffindor',
+  'logo_url' => 'https://cdn.shopify.com/s/files/1/0006/8213/1492/products/Gryffindor_Hogwarts_House_Crest_-_Harry_Potter_1024x1024.jpg'
+)
+gryffindor.save
+ravenclaw = House.new(
+  'name' => 'Ravenclaw',
+  'logo_url' => 'https://cdn.shopify.com/s/files/1/0006/8213/1492/products/Ravenclaw_Hogwarts_House_Crest_-_Harry_Potter_1024x1024.jpg'
+)
+ravenclaw.save
+hufflepuff = House.new(
+  'name' => 'Hufflepuff',
+  'logo_url' => 'https://cdn.shopify.com/s/files/1/0006/8213/1492/products/hufflepuff_1024x1024.jpg'
+)
+hufflepuff.save
+slytherin = House.new(
+  'name' => 'Slytherin',
+  'logo_url' => 'https://cdn.shopify.com/s/files/1/0006/8213/1492/products/Slytherin_Hogwarts_House_Crest_-_Harry_Potter_1024x1024.jpg'
+)
+slytherin.save
 
 Student.delete_all
 harry = Student.new(
